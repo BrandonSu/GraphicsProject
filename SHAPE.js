31ea35d6ca3e796370fb92c42366910374f1fedc
Brandon <bys221@nyu.edu>
2017-05-14 14:52:01 -0400

    updating

--- SHAPE.js
+++ SHAPE.js
@@ -30,7 +30,12 @@ var SHAPE = (function() {
       }
       return V;
    }
-
+ my.tube = function(u, v) {
+      var theta = 2 * Math.PI * u;
+      return [ Math.cos(theta),
+               Math.sin(theta),
+          2 * v - 1 ];
+   }
    function addTubeVertices(V, n) {
       return addMeshVertices(V, n, 1, function(u, v) {
          let theta = 2 * Math.PI * u;
@@ -40,13 +45,70 @@ var SHAPE = (function() {
          return [c,s,z, c,s,0, u,v];
       });
    }
-
    my.cylinder = function(n) {
       var V = [];
       addDiskVertices(V, n, -1, -1);
       addTubeVertices(V, n);
       addDiskVertices(V, n,  1,  1);
       return V;
+   }
+   function sphereVertices(V,n){
+      return addMeshVertices(V, n, 1, function(u, v) {
+         let theta = 2 * Math.PI * u;
+         let phi = Math.PI * (v-.5);
+         let x = Math.cos(theta)*Math.cos(phi);
+         let y = Math.sin(theta)*Math.cos(phi);
+         let z = Math.sin(phi);
+         return [x,y,z, x,y,0, u,v];
+      });
+   }
+//my attempt at matching his cylinder thing with a sphere tho
+   my.sphere = function(n){
+      var V=[];
+      sphereVertices(V,n);
+      return V;
+   }
+
+   my._quad = function(f, u0, v0, u1, v1) {
+      return [
+         f(u0, v0),
+         f(u1, v0),
+         f(u1, v1),
+         f(u0, v1),
+         f(u0, v0)
+      ];
+   }
+
+   my.parametricMesh = function(f, nu, nv) {
+      var i, j, u, v, C = [];
+      for (j = 0 ; j < nv ; j++) {
+         v = j / nv;
+         for (i = 0 ; i < nu ; i++) {
+            u = i / nu;
+       C.push(my._quad(f, u, v, u + 1/nu, v + 1/nv));
+         }
+      }
+      return C;
+   }
+
+   my.torus = function(u,v){
+      var theta = 2 * Math.PI * u;
+      var phi = 2 * Math.PI * v;
+      var r = 0.3;
+      return [Math.cos(theta)*(1+r*Math.cos(phi)), Math.sin(theta)*(1+r*Math.cos(phi)), r*Math.sin(phi)];
+   }
+
+   my.halfTorus = function(u,v){
+      var theta = 1 *Math.PI * u;
+      var phi = 2 * Math.PI * v;
+      var r = 0.3;
+      return [Math.cos(theta)*(1+r*Math.cos(phi)), Math.sin(theta)*(1+r*Math.cos(phi)), r*Math.sin(phi)];
+   }
+
+   my.halfSphere = function(u, v){
+      var theta = Math.PI * u;
+      var phi = Math.PI * (v-.5);
+      return [Math.cos(theta)*Math.cos(phi), Math.sin(theta)*Math.cos(phi), Math.sin(phi)];
    }
 
    return my;
